ebc43e22fea4406f209db45c42f4bfd560f75928
Devanshukoli <kolidevanshu02@gmail.com>
2026-06-08 12:51:38 +0530

    add easy challenge: implement solution to find the single number in an array

---
+++ easy/frequency/136_single_number.js
@@ -0,0 +1,45 @@
+/*
+Given a non-empty array of integers nums, every element appears twice except for one. Find that single one.
+
+You must implement a solution with a linear runtime complexity and use only constant extra space.
+
+ 
+
+Example 1:
+
+Input: nums = [2,2,1]
+
+Output: 1
+
+Example 2:
+
+Input: nums = [4,1,2,1,2]
+
+Output: 4
+
+Example 3:
+
+Input: nums = [1]
+
+Output: 1
+*/
+
+
+const singleNumber = (nums) => {
+  let freq = {};
+
+  for (let num of nums) {
+    freq[num] = (freq[num] || 0) + 1
+
+  }
+
+  for (let num of nums) {
+    
+    if (freq[num] === 1) {
+      return num;
+    }
+  }
+  
+};
+
+console.log(singleNumber([2, 2, 1]))
